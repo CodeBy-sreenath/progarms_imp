32817b5649c7558b8c0d9ab28093bd98cf2d3602
Sreenath T S <sreenathts655@gmail.com>
2025-12-31 01:20:54 +0530

    Add files via upload

---
+++ link_sum.py
@@ -0,0 +1,40 @@
+class ListNode:
+    def __init__(self,val=0,next=None):
+        self.val=val
+        self.next=next
+def add_two_numbers(l1,l2):
+    dummy=ListNode(0)
+    current=dummy
+    carry=0
+    while l1 or l2 or carry:
+        val1=l1.val if l1 else 0
+        val2=l2.val if l2 else 0
+        total=val1+val2+carry
+        carry=total//10
+        current.next=ListNode(total%10)
+        current=current.next
+        if l1:
+            l1=l1.next
+        if l2:
+            l2=l2.next
+            
+    return dummy.next
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+# l2 = 5 -> 6 -> 4
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+result=add_two_numbers(l1,l2)
+while result:
+    print(result.val,end="->")
+    result=result.next
+print(None)    
+                
+        
+        
+            
+        
+        
