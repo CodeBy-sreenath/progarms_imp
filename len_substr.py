067744088c7d06cd7dc6412a6ed70e453869c2b2
Sreenath T S <sreenathts655@gmail.com>
2025-12-31 01:44:28 +0530

    Add files via upload

--- len_substr.py
+++ len_substr.py
@@ -2,13 +2,15 @@ def longestsbstr(s):
     char_set=set()
     max_length=0
     left=0
+    start=0
     for right in range(len(s)):
         while s[right] in char_set:
             char_set.remove(s[left])
             left+=1
         char_set.add(s[right])
+        start=left
         max_length=max(max_length,right-left+1)
-    return max_length
-s="bbbb"
+    return s[start:start+max_length]
+s="abcabcabc"
 print(longestsbstr(s))        
         
